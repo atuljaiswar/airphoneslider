75b6eab67bd14a717b09077792a6476ba11577e9
atulj <atul.jaiswar@egrowisto.com>
2024-08-29 22:00:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
+const seeAll = document.querySelector('.back');
+const arrows = document.querySelector('.arrows');
+const sliderParent = document.querySelector('.sliders');
+const seeMoreList = document.querySelectorAll('.see-more');
+console.log(sliderParent.children);
+
+seeMoreList.forEach((item) => {
+  item.addEventListener('click', () => {
+    sliderParent.classList.add('showDetails');
+  });
+});
+
+next.addEventListener('click', () => {
+  showSlider('next');
+});
+
+prev.addEventListener('click', () => {
+  showSlider('prev');
+});
+
+const showSlider = (direction) => {
+  sliderParent.classList.remove('next1', 'prev1');
+  if (direction === 'next') {
+    console.log('HITTED', direction);
+    sliderParent.appendChild(sliderParent.children[0]);
+    sliderParent.classList.add('next1');
+  } else {
+    sliderParent.prepend(
+      sliderParent.children[sliderParent.children?.length - 1]
+    );
+    sliderParent.classList.add('prev1');
+  }
+};
+
+seeAll.addEventListener('click', () => {
+  sliderParent.classList.remove('showDetails');
+  arrows.classList.remove('direction');
+});
